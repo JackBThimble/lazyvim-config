ea616f30d303f8986389a8dd6286df4f1077696e
Jake Crowell <jake@localhost.localdomain>
2024-07-14 05:18:56 -0500

    removed dev-container plugin, added zig zls server

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,31 +23,92 @@ return {
       })
     end,
   },
-
-  -- or you can return new options to override all the defaults
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   event = "VeryLazy",
-  --   opts = function()
-  --     return {
-  --       --[[add your custom lualine config here]]
-  --     }
-  --   end,
-  -- },
-  --
-
   {
-    "https://codeberg.org/esensar/nvim-dev-container",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("devcontainer").setup({
-        init = true,
-        clean = true,
-        update = true,
-      })
-    end,
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        zls = {
+          cmd = { "/home/jake/.zvm/bin/zls" },
+        },
+      },
+    },
   },
 
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    lazy = true,
+    cmd = { "ToggleTerm" },
+    keys = {
+      {
+        "<leader>tf",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 0, LazyVim.root.get(), "float")
+        end,
+        desc = "ToggleTerm (float root_dir)",
+      },
+      {
+        "<leader>th",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 15, LazyVim.root.get(), "horizontal")
+        end,
+        desc = "ToggleTerm (horizontal root_dir)",
+      },
+      {
+        "<leader>tv",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, vim.o.columns * 0.4, LazyVim.root.get(), "vertical")
+        end,
+        desc = "ToggleTerm (vertical root_dir)",
+      },
+      {
+        "<leader>tn",
+        "<cmd>ToggleTermSetName<cr>",
+        desc = "Set term name",
+      },
+      {
+        "<leader>ts",
+        "<cmd>TermSelect<cr>",
+        desc = "Select term",
+      },
+      {
+        "<leader>tt",
+        function()
+          require("toggleterm").toggle(1, 100, LazyVim.root.get(), "tab")
+        end,
+        desc = "ToggleTerm (tab root_dir)",
+      },
+      {
+        [[<c-\>]],
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 15, LazyVim.root.get(), "horizontal")
+        end,
+      },
+    },
+    opts = {
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<c-\]],
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_size = true,
+      direction = "horizontal",
+      close_on_exit = true,
+    },
+  },
   {
     "max397574/better-escape.nvim",
     config = function()
